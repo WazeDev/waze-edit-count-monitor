88ae269c7515decad7460f41ec8b1dd79cef6dbe
mapomatic <mapomatic.waze@gmail.com>
2016-10-07 13:49:30 -0400

    Updated to latest version 0.9.2

--- Waze Edit Count Monitor.user.js
+++ Waze Edit Count Monitor.user.js
@@ -1,21 +1,19 @@
 // ==UserScript==
 // @name         Waze Edit Count Monitor
 // @namespace    
-// @version      0.8
+// @version      0.9.2
 // @description  Displays your daily edit count in the WME toolbar.  Warns if you might be throttled.
 // @author       MapOMatic
-// @include      https://editor-beta.waze.com/*editor/*
+// @include      https://beta.waze.com/*editor/*
 // @include      https://www.waze.com/*editor/*
 // @exclude      https://www.waze.com/*user/editor/*
-// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @grant        GM_xmlhttpRequest
+// @connect      www.waze.com
+
 // ==/UserScript==
 
-(function() {
-    var alertUpdate = true;
-    var wecmVersion = "0.8";
-    var wecmChangesHeader = "Waze Edit Count Monitor has been updated.\nv" + wecmVersion + "\n\nWhat's New\n-------------------------";
-    var wecmChanges = wecmChangesHeader + "\n- Added monitoring of UR count to reduce the number of false warnings.";
-    wecmChanges += wecmChangesHeader + "\n- UR count is displayed in the tool tip.";
+function WECM_Injected() {
     var debugLevel = 0;
     var $outputElem = null;
     var $outputElemContainer = null;
@@ -37,12 +35,7 @@
         var canSave = W.model.actionManager.canSave();
         var canRedo = W.model.actionManager.canRedo();
         if (lastCanSave && !canSave && !canRedo) {
-            $.ajax({url: 'https://www.waze.com/user/editor/' + userName,
-                    success: function(source){
-                        var profile = getEditorProfileFromSource(source);
-                        updateEditCount(getEditCountFromProfile(profile), getURCountFromProfile(profile));
-                    }
-                   });
+            window.postMessage(JSON.stringify(['wecmGetCounts',userName]),'*');
         }
         lastCanSave = canSave;
     }
@@ -51,26 +44,6 @@
     function loopCheck() {
         checkForSave();
         setTimeout(loopCheck, pollingTime);
-    }
-
-    function getEditorProfileFromSource(source) {
-        var match = source.match(/W.EditorProfile.data\s=\s+JSON.parse\('(.*?)'\)/i);
-        return JSON.parse(match[1]);
-    }
-
-    function getEditCountFromProfile(profile) {
-        var editingActivity = profile.editingActivity;
-        return editingActivity[editingActivity.length-1];
-    }
-
-    function getURCountFromProfile(profile) {
-        var editsByType = profile.editsByType;
-        for (i=0; i < editsByType.length; i++) {
-            if (editsByType[i].key == 'mapUpdateRequest') {
-                return editsByType[i].value;
-            }
-        }
-        return -1;
     }
 
     function updateEditCount(editCount, urCount) {
@@ -111,15 +84,24 @@
         lastURCount = urCount.count;
     }
 
+    function receiveMessage(event) {
+        var msg;
+        try {
+            msg = JSON.parse(event.data);
+        }
+        catch (err) {
+            // Do nothing
+        }
+
+        if (msg && msg[0] === "wecmUpdateUi") {
+            var editCount = msg[1][0];
+            var urCount = msg[1][1];
+            updateEditCount(editCount, urCount);
+        }
+    }
+
     function init() {
         'use strict';
-
-        /* Check version and alert on update */
-        if (alertUpdate && ('undefined' === window.localStorage.wecmVersion ||
-                            wecmVersion !== window.localStorage.wecmVersion)) {
-            alert(wecmChanges);
-            window.localStorage.wecmVersion = wecmVersion;
-        }
 
         userName = W.loginManager.user.userName;
         $outputElemContainer = $('<div>', {style:'border-radius: 23px; height: 23px; display: inline; float: right; padding-left: 10px; padding-right: 10px; margin: 9px 5px 8px 5px; font-weight: bold; font-size: medium;'});
@@ -137,6 +119,8 @@
             template: '<div class="tooltip" role="tooltip" style="opacity:0.95"><div class="tooltip-arrow"></div><div class="my-tooltip-header"><b></b></div><div class="my-tooltip-body tooltip-inner" style="font-weight: 600; !important"></div></div>'
         });
 
+        window.addEventListener('message', receiveMessage);
+
         loopCheck();
 
         log('Initialized.',0);
@@ -144,9 +128,12 @@
 
     function bootstrap()
     {
-        if (window.W && window.W.loginManager &&
-            window.W.loginManager.events.register &&
-            window.W.map && window.W.loginManager.isLoggedIn()) {
+        if (W &&
+            W.loginManager &&
+            W.loginManager.events &&
+            W.loginManager.events.register &&
+            W.map &&
+            W.loginManager.isLoggedIn()) {
             log('Initializing...', 0);
             init();
         } else {
@@ -158,5 +145,72 @@
     }
 
     bootstrap();
-})();
+}
 
+
+/* Code that is NOT injected into the page */
+(function(){
+    var alertUpdate = true;
+    var wecmVersion = GM_info.script.version;
+    var wecmChangesHeader = "Waze Edit Count Monitor has been updated.\nv" + wecmVersion + "\n\nWhat's New\n-------------------------";
+    var wecmChanges = wecmChangesHeader + "\n- Should now work in FF Greasemonkey and in WME beta.";
+
+    function getEditorProfileFromSource(source) {
+        var match = source.match(/W.EditorProfile.data\s=\s+JSON.parse\('(.*?)'\)/i);
+        return JSON.parse(match[1]);
+    }
+
+    function getEditCountFromProfile(profile) {
+        var editingActivity = profile.editingActivity;
+        return editingActivity[editingActivity.length-1];
+    }
+
+    function getURCountFromProfile(profile) {
+        var editsByType = profile.editsByType;
+        for (i=0; i < editsByType.length; i++) {
+            if (editsByType[i].key == 'mapUpdateRequest') {
+                return editsByType[i].value;
+            }
+        }
+        return -1;
+    }
+
+    // Listen for a message from the page.
+    function receiveMessage(event) {
+        var msg;
+        try {
+            msg = JSON.parse(event.data);
+        }
+        catch (err) {
+            // Do nothing
+        }
+
+        if (msg && msg[0] === "wecmGetCounts") {
+            var userName = msg[1];
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: 'https://www.waze.com/user/editor/' + userName,
+                onload: function(res) {
+                    var profile = getEditorProfileFromSource(res.responseText);
+                    window.postMessage(JSON.stringify(['wecmUpdateUi',[getEditCountFromProfile(profile), getURCountFromProfile(profile)]]),'*');
+                }
+            });
+        }
+    }
+
+    $(document).ready(function() {
+        /* Check version and alert on update */
+        if (alertUpdate && ('undefined' === window.localStorage.wecmVersion ||
+                            wecmVersion !== window.localStorage.wecmVersion)) {
+            alert(wecmChanges);
+            window.localStorage.wecmVersion = wecmVersion;
+        }
+    });
+
+    var WECM_Injected_script = document.createElement("script");
+    WECM_Injected_script.textContent = "" + WECM_Injected.toString() + " \n" + "WECM_Injected();";
+    WECM_Injected_script.setAttribute("type", "application/javascript");
+    document.body.appendChild(WECM_Injected_script);
+    window.addEventListener('message', receiveMessage);
+
+})();
