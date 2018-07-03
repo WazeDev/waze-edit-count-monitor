f8d55f01d9366180215cd8d5577a91e2b718b40f
mapomatic <mapomatic.waze@gmail.com>
2018-07-03 09:49:55 -0400

    update W.model.getModifiedObjects to W.model._getModifiedObjects.  Waze changed the function name to be private, so I need to figure out if there's a better method to use.

--- Waze Edit Count Monitor.js
+++ Waze Edit Count Monitor.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Waze Edit Count Monitor
 // @namespace    https://greasyfork.org/en/users/45389-mapomatic
-// @version      2018.05.27.001
+// @version      2018.07.03.001
 // @description  Displays your daily edit count in the WME toolbar.  Warns if you might be throttled.
 // @author       MapOMatic
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
@@ -51,7 +51,7 @@ function WECM_Injected() {
 
     function getChangedObjectCount() {
         let count = 0;
-        let changed = W.model.getModifiedObjects();
+        let changed = W.model._getModifiedObjects();
         Object.keys(changed).forEach(key => {
             let obj = changed[key];
             count += obj.Insert.length + obj.Update.length + obj.Delete.length;
