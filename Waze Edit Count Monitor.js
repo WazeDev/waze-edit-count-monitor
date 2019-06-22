c07f5c91abce77bbbd3569244f79b3a2c071854f
MapOMatic <mapomatic.waze@gmail.com>
2019-06-22 18:16:53 -0400

    version bump

--- Waze Edit Count Monitor.js
+++ Waze Edit Count Monitor.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Waze Edit Count Monitor
 // @namespace       https://greasyfork.org/en/users/45389-mapomatic
-// @version         2018.08.18.001
+// @version         2019.06.22.001
 // @description     Displays your daily edit count in the WME toolbar.  Warns if you might be throttled.
 // @author          MapOMatic
 // @include         /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
